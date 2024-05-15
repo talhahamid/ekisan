cc4de5afb745e0df0b9f20ae6142386da51d3112
talhahamid92 <talha.syed@oxcytech.com>
2024-05-15 06:29:41 +0300

    requriments.txt

--- ekisan/settings.py
+++ ekisan/settings.py
@@ -144,6 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
