bcdace4772bf9aa47662eda18f5ba91224114b11
Talha Hamid <talhahamid.syed@gmail.com>
2024-05-15 03:36:29 +0300

    Update settings.py

--- ekisan/settings.py
+++ ekisan/settings.py
@@ -13,7 +13,8 @@ from pathlib import Path
 import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,8 +136,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
-
+#STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
